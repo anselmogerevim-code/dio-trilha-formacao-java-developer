dc9fc9a203759aaca15f3c8395f5708a0475038f
Anselmo <anselmogerevim@gmail.com>
2026-07-13 21:21:21 -0300

    Atualização subindo projeto

---
+++ sistema-smart-tv/src/SmartTv.java
@@ -0,0 +1,42 @@
+public class SmartTv {
+    boolean ligada = false;
+    int canal = 1;
+    int volume = 25;
+
+    public void mudarCanal(int novoCanal) {
+        canal = novoCanal;
+        System.out.println("Canal alterado para: " + canal);
+    }
+
+    public void aumentarCanal() {
+        canal++;
+        System.out.println("Aumentando canal para: " + canal);
+    }
+
+    public void diminuirCanal() {
+        canal--;
+        System.out.println("Diminuindo canal para: " + canal);
+    }
+
+    public void mudarCanal() {
+
+    }
+
+    public void aumentarVolume() {
+        System.out.println("Aumentando o volume para: " + (volume + 1));
+        volume++;
+    }
+
+    public void diminuirVolume() {
+        System.out.println("Diminuindo o volume para: " + (volume - 1));
+        volume--;
+    }
+
+    public void ligar() {
+        ligada = true;
+    }
+
+    public void desligar() {
+        ligada = false;
+    }
+}
---
+++ sistema-smart-tv/src/Usuario.java
@@ -0,0 +1,31 @@
+public class Usuario {
+    public static void main(String[] args) {
+
+        SmartTv smarTv = new SmartTv();
+
+        System.out.println("A TV está ligada? " + smarTv.ligada);
+        System.out.println("Canal atual: " + smarTv.canal);
+        System.out.println("Volume atual: " + smarTv.volume);
+
+        smarTv.aumentarVolume();
+        smarTv.aumentarVolume();
+        smarTv.aumentarVolume();
+        smarTv.diminuirVolume();
+
+        System.out.println("Novo volume: " + smarTv.volume);
+        
+        System.out.println("Canal atual: " + smarTv.canal);
+
+        smarTv.mudarCanal(13);
+        smarTv.aumentarCanal();
+        smarTv.aumentarCanal();
+
+        smarTv.ligar();
+        System.out.println("Novo Status -> TV está ligada? " + smarTv.ligada);
+
+        smarTv.desligar();
+        System.out.println("Novo Status -> TV está ligada? " + smarTv.ligada);
+
+    }
+
+}
