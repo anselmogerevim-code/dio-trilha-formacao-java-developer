c60508983d66f995f4dee586c023326eeb08ce1a
Anselmo <anselmogerevim@gmail.com>
2026-07-21 12:50:59 -0300

    Inserindo demais desafios

--- controle-fluxo/src/ExemploForArray.java
+++ controle-fluxo/src/ExemploForArray.java
@@ -2,7 +2,7 @@ public class ExemploForArray {
     public static void main(String[] args) {
         String alunos [] = {"Felipe", "Jonas", "Julia", "Marcos"};
 
-        for (int x = 0; x < alunos.length; x++) {
+        for (int x = 1; x < alunos.length; x++) {
             System.out.println("O aluno no indice x=" + x + " é: " + alunos[x]);
         }
 
